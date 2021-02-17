8ad5e8b50c2355f3012f3b0c92b8b5143d6c6bad
AlessandroPuddu <alessandro.puddu94@gmail.com>
2021-02-17 15:07:53 +0100

    Spawner Ai start

---
+++ GameraTechTest/Assets/Scripts/SpawnerSystem/SpawnerAI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AI;
+using UnityEngine;
+
+namespace SpawnerSystem
+{
+    public class SpawnerAI : MonoBehaviour
+    {
+        private Fsm _fsm;
+
+        private void Awake()
+        {
+            FsmState wander = new FsmState();
+            FsmState flee = new FsmState();
+
+            _fsm = new Fsm(wander);
+        }
+    }
+}
