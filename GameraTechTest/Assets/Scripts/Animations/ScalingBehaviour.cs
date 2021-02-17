18bdd328815b5cd31ffd02d4a87caa876dea3b6b
AlessandroPuddu <alessandro.puddu94@gmail.com>
2021-02-17 17:40:10 +0100

    Pickups scaling

---
+++ GameraTechTest/Assets/Scripts/Animations/ScalingBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Animations
+{
+    public class ScalingBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float scalingSpeed;
+        
+        private void OnEnable()
+        {
+            StartCoroutine(ScalingCoroutine());
+        }
+
+        private IEnumerator ScalingCoroutine()
+        {
+            transform.localScale = new Vector3(0.1f, 0.1f,0.1f);
+
+            float timer = 0f;
+            
+            while (timer * scalingSpeed <= 1f)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, timer * scalingSpeed);
+
+                yield return null;
+                
+                timer += Time.deltaTime;
+            }
+        }
+    }
+}
