cf746c7987bc6959ab8a083b5c06a23454eda4b0
AlessandroPuddu <alessandro.puddu94@gmail.com>
2021-02-17 15:19:38 +0100

    Spawner FSM

--- GameraTechTest/Assets/Scripts/SpawnerSystem/SpawnerAI.cs
+++ GameraTechTest/Assets/Scripts/SpawnerSystem/SpawnerAI.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using System.Collections;
 using AI;
+using CommunicationSystem.Channels.Events;
+using CommunicationSystem.Channels.Variables;
 using UnityEngine;
 
 namespace SpawnerSystem
 {
     public class SpawnerAI : MonoBehaviour
     {
+        [SerializeField] private VoidEventChannel onWanderEnter;
+        [SerializeField] private VoidEventChannel onFleeEnter;
+
+        [SerializeField] private TransformChannel playerTransform;
+
+        [SerializeField] private float fleeDistance;
+        [SerializeField] private float wanderDistance;
+
+        [SerializeField] private float fsmResolution;
+        
         private Fsm _fsm;
 
         private void Awake()
@@ -13,7 +25,41 @@ namespace SpawnerSystem
             FsmState wander = new FsmState();
             FsmState flee = new FsmState();
 
+            FsmTransition t1 = new FsmTransition(PlayerNear);
+            FsmTransition t2 = new FsmTransition(PlayerFar);
+            
+            wander.AddTransition(t1,flee);
+            flee.AddTransition(t2,wander);
+            
+            wander.enterActions.Add(() => onWanderEnter.GetValue()?.Invoke());
+            flee.enterActions.Add(() => onFleeEnter.GetValue()?.Invoke());
+
             _fsm = new Fsm(wander);
+        }
+
+        private void Start()
+        {
+            StartCoroutine(UpdateFsm());
+        }
+
+        private bool PlayerNear()
+        {
+            return Vector3.Distance(transform.position, playerTransform.GetValue().position) <= fleeDistance;
+        }
+
+        private bool PlayerFar()
+        {
+            return Vector3.Distance(transform.position, playerTransform.GetValue().position) >= wanderDistance;
+        }
+
+        private IEnumerator UpdateFsm()
+        {
+            while (true)
+            {
+                _fsm.UpdateFsm();
+
+                yield return new WaitForSeconds(fsmResolution);
+            }
         }
     }
 }
