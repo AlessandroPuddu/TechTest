2152c0b8495061a786a13a13a6bd2225c11aa25b
AlessandroPuddu <alessandro.puddu94@gmail.com>
2021-02-17 17:34:17 +0100

    Pickup rotation

---
+++ GameraTechTest/Assets/Scripts/Animations/RotationBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animations
+{
+    public class RotationBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeed;
+        
+        private void Update()
+        {
+            float rotation = 6.0f * rotationSpeed * Time.deltaTime;
+            
+            transform.Rotate(rotation,rotation,rotation);
+        }
+    }
+}
