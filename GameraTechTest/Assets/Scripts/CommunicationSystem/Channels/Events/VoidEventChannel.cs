6de1fa07f1ef988ce2280361dc91576d1fd94447
AlessandroPuddu <alessandro.puddu94@gmail.com>
2021-02-17 15:12:17 +0100

    Void Event channel and listener

---
+++ GameraTechTest/Assets/Scripts/CommunicationSystem/Channels/Events/VoidEventChannel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CommunicationSystem.Channels.Events
+{
+    [CreateAssetMenu(fileName = "VoidEventChannel", menuName = "Channels/Events/VoidEventChannel", order = 0)]
+    public class VoidEventChannel : AbstractChannel<UnityAction>
+    {
+        public override void Add(UnityAction toAdd)
+        {
+            value += toAdd;
+        }
+
+        public override void Remove(UnityAction toRemove)
+        {
+            value -= toRemove;
+        }
+
+        public override void SetValue(UnityAction newValue)
+        {
+        }
+    }
+}
---
+++ GameraTechTest/Assets/Scripts/CommunicationSystem/Listeners/VoidEventListener.cs
@@ -0,0 +1,30 @@
+﻿using CommunicationSystem.Channels.Events;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CommunicationSystem.Listeners
+{
+    public class VoidEventListener : MonoBehaviour
+    {
+        [SerializeField] private VoidEventChannel channel;
+
+        [SerializeField] private UnityEvent onEventRaised;
+
+        private void OnEnable()
+        {
+            if (channel != null)
+                channel.Add(Respond);
+        }
+
+        private void OnDisable()
+        {
+            if (channel != null)
+                channel.Remove(Respond);
+        }
+
+        private void Respond()
+        {
+            onEventRaised?.Invoke();
+        }
+    }
+}
